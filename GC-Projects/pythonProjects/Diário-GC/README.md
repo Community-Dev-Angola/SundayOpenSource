bef0b47f4336e0ef2d0cd4c7a15c5d35c966f92b
Nurul-GC <Nurul-GC@github.com>
2021-01-24 17:29:10 -0100

    updating commit

---
+++ GC-Projects/pythonProjects/Diário-GC/README.md
@@ -0,0 +1,15 @@
+# Apresento-te o Diário-GC
+
+```
+um script-programa que tem a finalidade de REGISTRAR, EDITAR e LER
+mensagens ou pensamentos de forma simples e segura
+(já que ele também as codifica)
+usei um algoritmo de criptografia que tenho desenvolvido a algum tempo
+e que até a data presente me pareceu ser inquebrável.. (^_^)
+```
+
+![video-apresentacao](img/diariogc.mp4)
+
+---
+&copy; 2019-2021 [Nurul Carvalho](mailto:nuruldecarvalho@gmail.com) \
+&trade; [ArtesGC](https://artesgc.home.blog)
--- GC-Projects/pythonProjects/Diário-GC/diariogc.py
+++ GC-Projects/pythonProjects/Diário-GC/diariogc.py
@@ -8,17 +8,14 @@
 #  Allah no Comando.
 
 from os import mkdir, path
-from time import asctime
+import datetime
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.Qt import Qt, QSize, QCoreApplication
 from sys import argv
 from logging import *
 import webbrowser
-
-if not path.exists('DIARIOGC-{nome.text()}/debug'):
-    mkdir("DIARIOGC-{nome.text()}/debug")
-basicConfig(filename=f'DIARIOGC-{nome.text()}/debug/{asctime}.gc-log', level=INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+from secrets import token_bytes
 
 
 def isEmpty(string: str):
@@ -28,7 +25,23 @@ def isEmpty(string: str):
     return False
 
 
+def encrypt(p: str):
+    """funcao encriptadora"""
+    encriptar = p.encode()
+    encriptar_ = token_bytes(len(p))
 
+    encriptado = int.from_bytes(encriptar, 'big')
+    encriptado_ = int.from_bytes(encriptar_, 'big')
+
+    encriptado_final = encriptado ^ encriptado_
+    return encriptado_final, encriptado_
+
+
+def decrypt(p: int, q: int):
+    """funcao desencriptadora"""
+    palavra_encriptada = int(p) ^ int(q)
+    desencriptada = palavra_encriptada.to_bytes((palavra_encriptada.bit_length() + 7) // 8, 'big')
+    return desencriptada.decode()
 
 
 class DiarioGC:
@@ -38,6 +51,12 @@ class DiarioGC:
         self.ferramentas.setFixedSize(400, 400)
         self.ferramentas.setWindowTitle("Diario-GC")
         self.ferramentas.setPalette(QPalette(QColor("magenta")))
+        self.ferramentas.setWindowIcon(QIcon("img/diariogc.jpg"))
+
+        # variaveis extra
+        self.nome = None
+        self.janela5 = None
+        self.janela6 = None
 
         # criando menus para melhor configuração e explanação
         self.menu = QMenuBar(self.ferramentas)
@@ -59,11 +78,11 @@ class DiarioGC:
 
         self.abertura()
 
-    # @staticmethod
     def abertura(self):
         janela1 = QWidget()
 
         layout = QVBoxLayout()
+        layout.setSpacing(20)
 
         labelIntro = QLabel('<h1>Bem-Vindo</h1>')
         labelIntro.setAlignment(Qt.AlignCenter)
@@ -101,11 +120,13 @@ class DiarioGC:
         self.tab.addTab(janela1, "🙋")
         self.tab.setCurrentWidget(janela1)
 
-    # @staticmethod
     def cadastro(self):
         """funcao para cadastramento"""
         def guardar():
-            if senha.text() is not confirmarSenha.text():
+            if not path.exists(f'DIARIOGC-{self.nome.text()}/debug'):
+                mkdir(f"DIARIOGC-{self.nome.text()}/debug")
+            basicConfig(filename=f'DIARIOGC-{self.nome.text()}/debug/{datetime.datetime.today()}.gc-log', level=INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+            if senha.text() != confirmarSenha.text():
                 QMessageBox.critical(self.ferramentas, "Falha", f"Lamento {self.nome.text()} as senhas não correspondem..")
                 critical('senhas nao correspondem!')
                 pass
@@ -119,7 +140,7 @@ class DiarioGC:
                         mkdir(f'DIARIOGC-{self.nome.text()}')
                     with open(f'DIARIOGC-{self.nome.text()}/utilizador.log', 'w+') as salvarDados:
                         salvarDados.write(f"""*** Bem-Vindo ***
-Nome: {selfnome.text()}
+Nome: {self.nome.text()}
 Senha: {senha.text()}
 Resposta: {respostaEspecial.text()}""")
                         QMessageBox.information(self.ferramentas, "Bem-Vindo", "Cadastro Concluido..\nAgora inicie sessao para confirmar os seus dados!")
@@ -134,6 +155,7 @@ Resposta: {respostaEspecial.text()}""")
         self.ferramentas.setPalette(QPalette(QColor('blue')))
 
         layout = QFormLayout()
+        layout.setSpacing(20)
 
         labelImagem = QLabel()
         labelImagem.setPixmap(QPixmap("img/01.png"))
@@ -156,6 +178,7 @@ Resposta: {respostaEspecial.text()}""")
 
         respostaEspecial = QLineEdit()
         respostaEspecial.setToolTip('*: OBRIGATORIO!\nResposta especial que usaremos para recuperar o seu login..')
+        respostaEspecial.returnPressed.connect(guardar)
         layout.addRow('Qual seu bem mais precioso? *', respostaEspecial)
 
         botaoCadastrar = QPushButton('Cadastrar')
@@ -167,8 +190,8 @@ Resposta: {respostaEspecial.text()}""")
         self.tab.addTab(janela2, "Cadastro")
         self.tab.setCurrentWidget(janela2)
 
-    # @staticmethod
     def iniciar(self):
+        """funcao para inicio sessao"""
         def confirmar():
             if isEmpty(self.nome.text()) and isEmpty(senha.text()):
                 QMessageBox.critical(self.ferramentas, "Falha", "Lamento, mas você precisa preencher devidamente os seus dados antes de iniciar..")
@@ -180,9 +203,9 @@ Resposta: {respostaEspecial.text()}""")
                         confirmarDados = lerDados.read()
                         if (self.nome.text() in confirmarDados) and (senha.text() in confirmarDados):
                             self.tab.removeTab(0)
-                            return self.diario()
+                            return self.diario0()
                         elif (self.nome.text() not in confirmarDados) and (senha.text() not in confirmarDados):
-                            pergunta = QMessageBox.question(self.ferramentas, "Atenção", f"Lamento {self.nome.text()} voccê aind não tem uma conta..\nCadastre-se para ter acesso ao programa?")
+                            pergunta = QMessageBox.question(self.ferramentas, "Atenção", f"Lamento {self.nome.text()} você ainda não tem uma conta..\nCadastre-se para ter acesso ao programa..")
                             if pergunta == 1638:
                                 self.tab.removeTab(0)
                                 return self.cadastro()
@@ -191,9 +214,14 @@ Resposta: {respostaEspecial.text()}""")
                             else:
                                 pass
                         else:
-                            pass
+                            pergunta = QMessageBox.question(self.ferramentas, "Atenção", f"Lamento {self.nome.text()} você ainda não tem uma conta..\nCadastre-se para ter acesso ao programa..")
+                            if pergunta == 1638:
+                                self.tab.removeTab(0)
+                                return self.cadastro()
+                            elif pergunta == 65536:
+                                return self.gc.instance().exit()
                 except Exception as e:
-                    QMessageBox.warning(self.ferramentas, "Falha", f"Lamento ocorreu um erro inesperado:\n\t{e}\n\nTente verificar e preencher propriamente os seus dados..")
+                    QMessageBox.warning(self.ferramentas, "Falha", f"Lamento ocorreu um erro inesperado:\nTente verificar e preencher propriamente os seus dados..")
                     warning(f'{e}')
                     pass
 
@@ -201,6 +229,7 @@ Resposta: {respostaEspecial.text()}""")
         self.ferramentas.setPalette(QPalette(QColor('pink')))
 
         layout = QFormLayout()
+        layout.setSpacing(20)
 
         labelImagem = QLabel()
         labelImagem.setPixmap(QPixmap("img/03.png"))
@@ -208,18 +237,14 @@ Resposta: {respostaEspecial.text()}""")
         layout.addWidget(labelImagem)
 
         self.nome = QLineEdit()
-        self.fnome.setToolTip('*: OBRIGATORIO!')
+        self.nome.setToolTip('*: OBRIGATORIO!')
         layout.addRow('&Nome: *', self.nome)
 
         senha = QLineEdit()
         senha.setToolTip('*: OBRIGATORIO!')
         senha.setEchoMode(senha.PasswordEchoOnEdit)
+        senha.returnPressed.connect(confirmar)
         layout.addRow('&Senha: *', senha)
-
-        labelRecuperarSenha = QLabel('<a href="#" style="text-decoration:none;">Esqueceu a sua senha?</a>')
-        labelRecuperarSenha.linkActivated.connect(self.recuperarSenha)
-        labelRecuperarSenha.setAlignment(Qt.AlignRight)
-        layout.addWidget(labelRecuperarSenha)
 
         botaoIniciar = QPushButton('Iniciar')
         botaoIniciar.clicked.connect(confirmar)
@@ -227,67 +252,24 @@ Resposta: {respostaEspecial.text()}""")
         layout.addWidget(botaoIniciar)
 
         janela3.setLayout(layout)
-        self.tab.addTab(janela3, "Iniciar Sessão")
+        self.tab.addTab(janela3, "Iniciar-Sessão")
         self.tab.setCurrentWidget(janela3)
 
-    def recuperarSenha(self):
-        def confirmar():
-            if isEmpty(self.nome.text()) and isEmpty(respostaEspecial.text()):
-                QMessageBox.critical(self.ferramentas, "Falha", "Lamento, mas você precisa preencher devidamente os seus dados antes de iniciar..")
-                critical('dados nao preenchidos!')
-                pass
-            else:
-                try:
-                    with open(f'DIARIOGC-{self.nome.text()}/utilizador.log', 'r+') as lerDados:
-                        confirmarDados = lerDados.read()
-                        if (self.nome.text() in confirmarDados) and (respostaEspecial.text() in confirmarDados):
-                            self.tab.removeTab(0)
-                            return self.diario()
-                        elif (self.nome.text() not in confirmarDados) and (senha.text() not in confirmarDados):
-                            pergunta = QMessageBox.question(self.ferramentas, "Atenção", f"Lamento {self.nome.text()} voccê aind não tem uma conta..\nCadastre-se para ter acesso ao programa?")
-                            if pergunta == 1638:
-                                self.tab.removeTab(0)
-                                return self.cadastro()
-                            elif pergunta == 65536:
-                                return self.gc.instance().exit()
-                            else:
-                                pass
-                        else:
-                            pass
-                except Exception as e:
-                    QMessageBox.warning(self.ferramentas, "Falha", f"Lamento ocorreu um erro inesperado:\n\t{e}\n\nTente verificar e preencher propriamente os seus dados..")
-                    warning(f'{e}')
-                    pass
-
-        janela4 = QWidget()
-        janela4.setFixedSize(250, 250)
-        janela4.setPalette(QPalette(QColor('Wheat')))
-
-        layout = QFormLayout()
-
-        labelImagem = QLabel()
-        labelImagem.setPixmap(QPixmap("img/06.png"))
-        labelImagem.setAlignment(Qt.AlignCenter)
-        layout.addWidget(labelImagem)
-
-        self.nome = QLineEdit()
-        self.nome.setToolTip('*: OBRIGATORIO!')
-        layout.addRow('&Nome: *', self.nome)
-
-        respostaEspecial = QLineEdit()
-        respostaEspecial.setToolTip('*: OBRIGATORIO!\nResposta especial que usaremos para recuperar o seu login..')
-        layout.addRow('Qual seu bem mais precioso? *', respostaEspecial)
-
-        botaoIniciar = QPushButton("Iniciar")
-        botaoIniciar.clicked.connect(confirmar)
-        botaoIniciar.setDefault(True)
-        layout.addWidget(botaoIniciar)
-
-        janela4.setLayout(layout)
-        janela4.show()
+    def diario0(self):
+        if self.janela5 is None:
+            return self.diario()
+        try:
+            self.tab.setCurrentWidget(self.janela5)
+        except Exception as e:
+            self.tab.removeTab(0)
+            return self.diario()
+        else:
+            self.tab.removeTab(0)
+            return self.diario()
 
     def diario(self):
-        janela5 = QWidget()
+        """funcao pagina-principal do programa"""
+        self.janela5 = QWidget()
         self.ferramentas.setPalette(QPalette(QColor('antiquewhite')))
 
         layout = QFormLayout()
@@ -304,7 +286,7 @@ Resposta: {respostaEspecial.text()}""")
         layout.addRow(labelIntro)
 
         escreverBotao = QPushButton('Escrever')
-        escreverBotao.clicked.connect(self.codificar1)
+        escreverBotao.clicked.connect(self.escrever)
         escreverBotao.setDefault(True)
         layout.addRow(escreverBotao)
 
@@ -325,12 +307,25 @@ Resposta: {respostaEspecial.text()}""")
         labeLink.linkActivated.connect(browser)
         layout.addWidget(labeLink)
 
-        janela5.setLayout(layout)
-        self.tab.addTab(janela5, 'Pagina-Inicial')
-        self.tab.setCurrentWidget(janela5)
+        self.janela5.setLayout(layout)
+        self.tab.addTab(self.janela5, 'Pagina-Inicial')
+        self.tab.setCurrentWidget(self.janela5)
+
+    def escrever0(self):
+        if self.janela6 is None:
+            return self.escrever()
+        try:
+            self.tab.setCurrentWidget(self.janela6)
+        except Exception as e:
+            self.tab.removeTab(1)
+            return self.escrever()
+        else:
+            self.tab.removeTab(1)
+            return self.escrever()
 
     def escrever(self):
-        janela6 = QWidget()
+        """funcao para escrever um novo pensamento"""
+        self.janela6 = QWidget()
 
         layout = QVBoxLayout()
 
@@ -360,13 +355,14 @@ Resposta: {respostaEspecial.text()}""")
                         fileCodificado.write(str(doc1) + '\n' + str(doc2))
                         QMessageBox.information(self.ferramentas, 'Concluido', 'Codificação Bem Sucedida..')
                         self.tab.removeTab(1)
-                        self.main0()
+                        self.diario0()
                 except Exception as e:
                     QMessageBox.warning(self.ferramentas, "Falha", f"Lamento, ocorreu um erro inesperado..\n\t{e}")
                     warning(f'{e}')
 
         hl = QHBoxLayout()
-        guardarBotao = QPushButton('Guardar (Codificado)')
+        guardarBotao = QPushButton('Guardar')
+        guardarBotao.setToolTip('CODIFICADO')
         guardarBotao.clicked.connect(guardar)
         guardarBotao.setDefault(True)
         hl.addWidget(guardarBotao)
@@ -378,21 +374,23 @@ Resposta: {respostaEspecial.text()}""")
         hl.addWidget(cancelarBotao)
         layout.addLayout(hl)
 
-        janela6.setLayout(layout)
-        self.tab.addTab(janela6, 'Novo-Arquivo')
-        self.tab.setCurrentWidget(janela6)
+        self.janela6.setLayout(layout)
+        self.tab.addTab(self.janela6, 'Novo-Arquivo')
+        self.tab.setCurrentWidget(self.janela6)
 
     def ler(self):
+        """funcao para escolher um ficheiro ja escrito e abri-lo para ler"""
         nome_file_open = QFileDialog.getOpenFileName(parent=self.ferramentas, directory=f'DIARIOGC-{self.nome.text()}/pensamentos', filter='Ficheiros (*.gc-txt)')
         try:
             with open(nome_file_open[0], 'r+') as file_decod:
                 file_ = file_decod.readlines()
                 file = decrypt(file_[0], file_[1])
 
-            moldura_decod = QWidget()
+            janela7 = QWidget()
+
             layout = QVBoxLayout()
 
-            texto = QTextEdit(self.moldura_decod)
+            texto = QTextEdit()
             texto.setReadOnly(True)
             texto.setFont(QFont('cambria', 10))
             texto.insertPlainText(file)
@@ -403,24 +401,23 @@ Resposta: {respostaEspecial.text()}""")
             fecharBotao.clicked.connect(fechar)
             layout.addWidget(fecharBotao)
 
-            moldura_decod.setLayout(layout)
-            self.tab.addTab(moldura_decod, 'Lendo-Arquivo')
-            self.tab.setCurrentWidget(moldura_decod)
+            janela7.setLayout(layout)
+            self.tab.addTab(janela7, 'Lendo-Arquivo')
+            self.tab.setCurrentWidget(janela7)
         except FileNotFoundError:
             QMessageBox.warning(self.ferramentas, 'Aviso', 'Ficheiro Não Encontrado ou Processo Cancelado!')
+            warning('ficheiro nao encontrado ou processo cancelado!')
 
     def editar(self):
-        self.utilizador = (self.utilizador_is.text() or self.utilizador_cd.text())
-        nome_file_open = QFileDialog.getOpenFileName(parent=self.ferramentas, directory=f'C9R{self.utilizador}', filter='Ficheiros (*.gc)')
+        """funcao para escolher um ficheiro ja escrito e abri-lo para editar"""
+        nome_file_open = QFileDialog.getOpenFileName(parent=self.ferramentas, directory=f'DIARIOGC-{self.nome.text()}/pensamentos', filter='Ficheiros (*.gc-txt)')
         try:
             with open(nome_file_open[0], 'r+') as file_decod:
                 file_ = file_decod.readlines()
                 file = decrypt(file_[0], file_[1])
 
-            self.moldura_editar = QFrame()
-            self.moldura_editar.setPalette(QPalette(QColor('Wheat')))
-            self.tab.addTab(self.moldura_editar, 'Editando Arquivo')
-            self.tab.setCurrentWidget(self.moldura_editar)
+            janela8 = QWidget()
+
             layout = QVBoxLayout()
 
             texto = QTextEdit()
@@ -435,25 +432,45 @@ Resposta: {respostaEspecial.text()}""")
 
                 QMessageBox.information(self.ferramentas, 'Concluido', 'Codificação Bem Sucedida..')
                 self.tab.removeTab(1)
-                self.main0()
+                self.diario0()
 
-            guardar_botao = QPushButton('Guardar (Recodificado)')
-            guardar_botao.clicked.connect(guardar)
-            layout.addWidget(guardar_botao)
+            guardarBotao = QPushButton('Guardar')
+            guardarBotao.setToolTip('RECODIFICADO')
+            guardarBotao.clicked.connect(guardar)
+            layout.addWidget(guardarBotao)
 
             fechar = lambda: self.tab.removeTab(1)
             fechar_botao = QPushButton('Fechar')
             fechar_botao.clicked.connect(fechar)
             layout.addWidget(fechar_botao)
-            self.moldura_editar.setLayout(layout)
+
+            janela8.setLayout(layout)
+            self.tab.addTab(janela8, 'Editando-Arquivo')
+            self.tab.setCurrentWidget(janela8)
         except FileNotFoundError:
-            QMessageBox.warning(self.ferramentas, 'Aviso', 'Ficheiro Não Encontrado ou Processo Cancelado!')
+            QMessageBox.warning(self.ferramentas, 'Falha', 'Ficheiro Não Encontrado ou Processo Cancelado!')
+            warning('ficheiro nao encontrado ou processo cancelado!')
 
     def instrucoes(self):
-        QMessageBox.information(self.ferramentas, "Instruções", "")
+        QMessageBox.information(self.ferramentas, "Instruções", """
+Apresento-te o Novo e Reformulado Diario-GC
+Um script-programa que tem a finalidade de REGISTRAR, EDITAR e LER
+mensagens ou pensamentos de forma simples e segura
+(já que ele também as codifica) usei um algoritmo
+de criptografia que tenho desenvolvido a algum tempo
+e que até a data presente me pareceu ser inquebrável.. (^_^)
+
+(c) 2019-2021 Nurul GC
+(TM) ArtesGC
+""")
 
     def sobre(self):
-        QMessageBox.information(self.ferramentas, "Sobre", "")
+        QMessageBox.information(self.ferramentas, "Sobre", """
+Nome: Diario-GC
+Versão: 0.9.012021
+Programador & Designer: Nurul GC
+Empresa: ArtesGC Inc
+""")
 
 
 if __name__ == '__main__':
